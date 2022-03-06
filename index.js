e7ea2e2d4e5e5e5bc459817ee76e1d8e3554c771
Alex <jzsplking@gmail.com>
2022-03-06 18:07:04 +0800

    :refactor: refactor dev code

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // import {createGraph, bundle} from './src/minipack'
 const {createGraph,bundle} = require('./src/minipack')
 const fs = require('fs');
+const rimraf = require('rimraf');
 
 const graph = createGraph('./example/index.js');
 
 const res = bundle(graph);
 
-fs.mkdirSync('./dist');
+fs.rmSync('dist', { recursive: true, force: true });
+fs.mkdirSync('dist');
 fs.writeFile('./dist/bundle.js', res, (res) => {
     console.log('write bundle file res', res)
 })
