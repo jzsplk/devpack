b89bf8186ac1a3cd75f8b731ba9a579ca0393b2d
Alex <jzsplking@gmail.com>
2022-03-06 18:02:11 +0800

    Change dev index file

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // import {createGraph, bundle} from './src/minipack'
 const {createGraph,bundle} = require('./src/minipack')
+const fs = require('fs');
 
 const graph = createGraph('./example/index.js');
 
 const res = bundle(graph);
+
+fs.mkdirSync('./dist');
+fs.writeFile('./dist/bundle.js', res, (res) => {
+    console.log('write bundle file res', res)
+})
 
 console.log('result', res);
